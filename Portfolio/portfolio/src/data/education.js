655e164e9e0e2488d36b8d17c9575688c065336b
Rohitha0105 <160598339+Rohitha0105@users.noreply.github.com>
2026-01-14 19:49:40 +0530

    Update education.js

--- Portfolio/portfolio/src/data/education.js
+++ Portfolio/portfolio/src/data/education.js
@@ -2,5 +2,6 @@ export const EDUCATION = {
   college: "NRI Institute of Technology",
   degree: "B.Tech – CSE (Artificial Intelligence & Machine Learning)",
   years: "September 2022 - April 2026",
-  cgpa: "7.6 / 10"
+  cgpa: "7.81 / 10"
 };
+
